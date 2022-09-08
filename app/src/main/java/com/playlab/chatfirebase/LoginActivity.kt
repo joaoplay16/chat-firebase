b95be1d1fcf9e5c2d82702c4a4de88b69ece3f92
João Pedro <joaoplay16@gmail.com>
2022-09-08 19:18:52 -0300

    LoginActivity binding views

--- app/src/main/java/com/playlab/chatfirebase/LoginActivity.kt
+++ app/src/main/java/com/playlab/chatfirebase/LoginActivity.kt
@@ -2,10 +2,21 @@ package com.playlab.chatfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.playlab.chatfirebase.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        with(binding){
+            btnEnter.setOnClickListener {
+                val email = edtEmail.text
+                val password = edtPassword.text
+            }
+        }
     }
 }
