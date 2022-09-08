a81c9a1c671713ec2953e43bb9f82fd681d278e8
João Pedro <joaoplay16@gmail.com>
2022-09-08 19:28:47 -0300

    register activity created

---
+++ app/src/main/java/com/playlab/chatfirebase/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package com.playlab.chatfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
