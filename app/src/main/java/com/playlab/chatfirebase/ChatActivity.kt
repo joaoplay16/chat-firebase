bb0b9583812cffb06f4cc74079ac32711e8b470a
João Pedro <joaoplay16@gmail.com>
2022-09-10 16:05:06 -0300

    implementing chat adapter

--- app/src/main/java/com/playlab/chatfirebase/ChatActivity.kt
+++ app/src/main/java/com/playlab/chatfirebase/ChatActivity.kt
@@ -2,10 +2,41 @@ package com.playlab.chatfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Adapter
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.playlab.chatfirebase.databinding.ActivityChatBinding
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
 
 class ChatActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChatBinding
+    private lateinit var adapter: GroupAdapter<GroupieViewHolder>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
+        binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        adapter = GroupAdapter<GroupieViewHolder>()
+        binding.recyclerChat.layoutManager = LinearLayoutManager(this)
+        binding.recyclerChat.adapter = adapter
+
+        adapter.add(MessageItem(isLeft = true))
+        adapter.add(MessageItem(isLeft = false))
+        adapter.add(MessageItem(isLeft = true))
+        adapter.add(MessageItem(isLeft = false))
+        adapter.add(MessageItem(isLeft = false))
+        adapter.add(MessageItem(isLeft = true))
+    }
+
+    private class MessageItem(private var isLeft: Boolean): Item<GroupieViewHolder>(){
+
+        override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+        }
+
+        override fun getLayout() =
+            if (isLeft) R.layout.item_from_message else R.layout.item_to_message
     }
 }
