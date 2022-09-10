dcd94b9030948d23fc7cb55f4c198444e5646727
João Pedro <joaoplay16@gmail.com>
2022-09-09 21:29:46 -0300

    picking photo

--- app/src/main/java/com/playlab/chatfirebase/RegisterActivity.kt
+++ app/src/main/java/com/playlab/chatfirebase/RegisterActivity.kt
@@ -1,14 +1,35 @@
 package com.playlab.chatfirebase
 
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
 import com.playlab.chatfirebase.databinding.ActivityRegisterBinding
+import java.io.IOException
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+
+    val launcherImage = registerForActivityResult(
+        ActivityResultContracts.GetContent()
+    ) { uri ->
+        try {
+
+        }catch (e: IOException){
+            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            val bitmapDrawable = BitmapDrawable(resources, bitmap)
+            binding.btnSelectPhoto.alpha = 0.0f
+            binding.imgPhoto.setImageDrawable(bitmapDrawable)
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
@@ -18,9 +39,15 @@ class RegisterActivity : AppCompatActivity() {
             btnEnter.setOnClickListener {
                 createUser()
             }
+
+            btnSelectPhoto.setOnClickListener {
+                selectPhoto()
+            }
         }
+    }
 
-
+    private fun selectPhoto() {
+        launcherImage.launch("image/*")
     }
 
     private fun createUser(){
