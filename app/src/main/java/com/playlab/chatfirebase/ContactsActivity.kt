044f71215c8268bce6483f4c61760ffa92c8a2e2
João Pedro <joaoplay16@gmail.com>
2022-09-10 14:07:44 -0300

    fetch users

--- app/src/main/java/com/playlab/chatfirebase/ContactsActivity.kt
+++ app/src/main/java/com/playlab/chatfirebase/ContactsActivity.kt
@@ -2,10 +2,36 @@ package com.playlab.chatfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.EventListener
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.QuerySnapshot
 
 class ContactsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
+
+
+        fetchUsers()
+    }
+
+    private fun fetchUsers() {
+        FirebaseFirestore.getInstance().collection("/users")
+            .addSnapshotListener { querySnapshot, error ->
+                if (error != null){
+                    Log.e("LOGGER", error.message.toString(), error)
+                    return@addSnapshotListener
+                }
+
+                val docs: List<DocumentSnapshot>? = querySnapshot?.documents
+                if (docs != null) {
+                    for (doc in docs){
+                        val user = doc.toObject(User::class.java)
+                        Log.d("LOGGER", user?.userName.toString())
+                    }
+                }
+            }
     }
 }
