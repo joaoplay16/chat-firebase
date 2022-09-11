8eb96be11bb66701a13bcba733472cb30f489c7e
João Pedro <joaoplay16@gmail.com>
2022-09-11 16:24:34 -0300

    hiding app user

--- app/src/main/java/com/playlab/chatfirebase/MyApplication.kt
+++ app/src/main/java/com/playlab/chatfirebase/MyApplication.kt
@@ -1,11 +1,54 @@
 package com.playlab.chatfirebase
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
-class MyApplication : Application() {
+class MyApplication : Application(), Application.ActivityLifecycleCallbacks {
     override fun onCreate() {
         super.onCreate()
         FirebaseApp.initializeApp(this)
     }
+
+    private fun setOnline(online: Boolean){
+        val uid = FirebaseAuth.getInstance().uid
+
+        if(uid != null){
+            FirebaseFirestore.getInstance().collection("users")
+                .document(uid)
+                .update("online", online)
+        }
+    }
+
+    override fun onActivityCreated(p0: Activity, p1: Bundle?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityStarted(p0: Activity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityResumed(p0: Activity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityPaused(p0: Activity) {
+        setOnline(false)
+    }
+
+    override fun onActivityStopped(p0: Activity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActivityDestroyed(p0: Activity) {
+        TODO("Not yet implemented")
+    }
+
 }
