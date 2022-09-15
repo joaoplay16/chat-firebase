0e4f61b33b340021da793a924ae18e346a0bcdf4
João Pedro <joaoplay16@gmail.com>
2022-09-14 22:27:30 -0300

    make contact parcelable

--- app/src/main/java/com/playlab/chatfirebase/Contact.kt
+++ app/src/main/java/com/playlab/chatfirebase/Contact.kt
@@ -1,12 +1,16 @@
 package com.playlab.chatfirebase
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Contact(
     val uuid: String,
     val username: String,
     val lastMessage: String,
     val timestamp: Long,
     val photoUrl: String
-    ) {
+    ): Parcelable {
 
     constructor(): this("","",
         "",0, "")
